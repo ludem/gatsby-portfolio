f09a7a02bb5649d144eaca0c4d117fd71667f163
Luca D'Emilio <luca.demilio@capgemini.com>
2018-11-16 16:31:19 +0100

    Fixed netifly plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,6 @@ module.exports = {
     },
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-remark`,
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
-    }
+    `gatsby-plugin-netlify-cms`
   ]
 };
